5f0166ef755074f70fee983684ac9035cc2af097
Shehan Reshin <45056483+shehanreshin@users.noreply.github.com>
2023-05-26 21:09:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AniList-Random-Generator
+ A web application that randomly selects a manga or anime from an AniList user's planned lists
---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://anilist.co/user/polkura/mangalist/Planning'
+html = requests.get(url)
+
+scraper = BeautifulSoup(html.content, 'html.parser')
+
+entry_rows = scraper.find_all('div', class_='entry row')
+titles = []
+progress_arr = []
+formats = []
+
+for row in entry_rows:
+    
+    title_div = row.find('div', class_='title')
+    if title_div:
+        title_content = title_div.text.strip()
+        titles.append(title_content)
+
+    progress_div = row.find('div', class_='progress')
+    if progress_div:
+        progress_content = progress_div.text.strip().replace('\n+','')
+        progress_arr.append(progress_content)
+
+    format_div = row.find('div', class_='format')
+    if format_div:
+        format_content = format_div.text.strip()
+        formats.append(format_content)
+
+planned = []
+
+for num in range(len(titles)):
+    entry = {'title':titles[num], 'progress':progress_arr[num], 'format':formats[num]}
+    planned.append(entry)
+
+for entry in planned:
+    if entry['title'] == 'Adam to Eve':
+        print(entry)
